2d27b4876a700a41f174c04a807a26ccfeecc5a0
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2022-06-24 09:51:23 +0530

    Update Program.cs
    Fibonacci code added

--- Program.cs
+++ Program.cs
@@ -139,7 +139,20 @@ namespace InterviewQuestion
 
         }
 
+	#region Fibonacii
+	  public static void Fibonacii(int number)
+          {
+		    int n1 = 0, n2 = 1, n3, i;
 
+		    for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+		    {
+			n3 = n1 + n2;
+			Debug.Log(n3 + " ");
+			n1 = n2;
+			n2 = n3;
+		    }
+          }
+	#endRegion
 
 
         #endregion
